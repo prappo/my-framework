7a9d8b206d199e11416b000fda616a9ae47584de
Prappo Prince <prappo.prince@gmail.com>
2019-09-03 15:57:06 +0600

    get start

---
+++ app/Http/Middleware/Install.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Request;
+
+class Install
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!isInstalled()) {
+            if (!(Request::is('install') || Request::is('install/*'))) {
+
+                return redirect('/install');
+
+            }
+
+        }
+        return $next($request);
+    }
+}
